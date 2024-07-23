1ee242cc5850e515a4e3b92dcbf5187e6bb40443
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2024-07-23 12:49:05 -0700

    initial commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -15,7 +15,20 @@ const Main = () => {
             </div>
             <div className="cards">
                 <div className="card">
-                    
+                    <p>Suggest beautiful places to visit</p>
+                    <img src={assets.compass_icon} alt=''/>
+                </div>
+                <div className="card">
+                    <p>Briefly Summarize:urban plannig</p>
+                    <img src={assets.bulb_icon} alt=''/>
+                </div>
+                <div className="card">
+                    <p>Brainstorm Team bonding activites for our work retreat</p>
+                    <img src={assets.message_icon} alt=''/>
+                </div>
+                <div className="card">
+                    <p>Improve the readibilty of the following code</p>
+                    <img src={assets.code_icon} alt=''/>
                 </div>
             </div>
         </div>
