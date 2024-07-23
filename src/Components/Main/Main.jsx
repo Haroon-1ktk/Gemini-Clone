dfdb65b477dbd312f136535e3e177a9817269abe
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2024-07-23 13:14:01 -0700

    initial commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -31,6 +31,19 @@ const Main = () => {
                     <img src={assets.code_icon} alt=''/>
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter your prompt here...' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    Gemini may provide inaccurate info about people places etc please double check it
+                </p>
+            </div>
         </div>
     </div>
   )
